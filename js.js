c46d8b0c74218bf012bcc0cf7403d74a5abe0dfe
Charlotte <147110250+CharlotteStorey64@users.noreply.github.com>
2025-05-22 18:06:01 +0000

    im so tired

--- js.js
+++ js.js
@@ -1,12 +1,13 @@
-var list = [
-    "Emily",
-    "not Emily",
-    "maybe Emily"
-];
-
+//making buttons and cloning them plus some other stuff
 var imgs = ["octopus.png","wolf.jpg", "america-4.png", "weevil.png","trainbaby.png"];
 
 var btn = document.getElementById("btn");
+
+var doThing = function(event){
+    console.log(event);
+    alert("Ouch! Don't click so hard :(")
+
+}
 
 for(var j = 0; j < imgs.length; j++){
     var pic = document.getElementById(imgs[j]);
@@ -15,13 +16,7 @@ for(var j = 0; j < imgs.length; j++){
 
     var newBtn = btn.cloneNode(true);
     document.body.append(newBtn);
+    newBtn.addEventListener("click", doThing);
     
 
 }
-/*
-for(var i = 0; i < 3; i++){
-    var newBtn = btn.cloneNode(true);
-    document.body.append(newBtn);
-    alert("HELL YEAH BROTHER!!!!!!!! " + list[i]);
-}
-*/
